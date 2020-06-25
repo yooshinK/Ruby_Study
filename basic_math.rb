79115cb4509c908930a495dcb2771e77e01bca7b
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-06-25 22:55:28 +0900

    Add files via upload

---
+++ basic_math.rb
@@ -0,0 +1,19 @@
+print(10+5)
+print(10-5)
+print(10*5)
+print(10/5)
+print(10%5)
+
+puts(" ")
+puts(10+5)
+puts(10-5)
+puts(10*5)
+puts(10/5)
+puts(10%5)
+
+#Ruby doesn't require 'import'
+puts(2.24.ceil())
+puts(29.9.floor())
+puts((2).pow(10)) # ==
+puts(2**10)
+puts(Math::PI) # = 3.141592653589793
