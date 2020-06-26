c21cca2c212928f4f6bf2515a8dd3f73d0e3dfb1
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-06-26 23:24:20 +0900

    Add files via upload

---
+++ string_test.rb
@@ -0,0 +1,21 @@
+puts('Hello '+'ruby')
+puts('Hello '+'python '*3)
+
+# '-', '/' cause an error - but Python % cause an error
+#puts('Hello '+'python %%'/3)
+
+# array puts('Hello ' [0])
+puts('Ruby' [1])
+
+#To make the first latter an Upper latter
+puts('aaaaaaaaaaa'.capitalize())
+
+#To make the whole sentence Upper latters
+puts('aaaaaaaaaaa'.upcase())
+
+#To get the length of the sentence
+puts('aaaaaaaaaaa'.length())
+# print(length('aaaaaaaaaaaA')) Error!
+
+# To substitute
+puts('Hello World'.sub('orld','Ruby'))
