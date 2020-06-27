2c90da6584d8cba06429f8bdb8c2bde0332fa51b
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-06-27 12:35:52 +0900

    Add files via upload

---
+++ Variable.rb
@@ -0,0 +1,12 @@
+x=100
+y=5
+puts(x+y)
+
+title = "Photography"
+#Converting Data to Strings .to_s
+puts("Story"+title+x.to_s)
+
+#Converting Strings to Numbers to_i and to_f
+puts("5".to_i)
+puts("5".to_f)
+puts("5.8".to_i) #
