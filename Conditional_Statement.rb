767fdc1cfa56211448c7b7ae060652cb3bab8342
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-06-29 20:43:16 +0900

    Add files via upload

---
+++ Conditional_Statement.rb
@@ -0,0 +1,16 @@
+if false #루비는 전부 소문자
+  puts("Ruby True1")
+end #end로 영역 구분
+puts("Ruby True2")
+puts("Ruby True3")
+
+input = 22
+real = 11
+
+if input == real
+  puts("the same")
+elsif real != input # else if -> elsif
+  puts("different")
+else
+  puts("Nothing")
+end
