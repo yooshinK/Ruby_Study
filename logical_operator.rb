6636f1d8ba7c68cfb7fbf9ac8e5f48a9015bdbbe
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-07-01 21:30:12 +0900

    Add files via upload

---
+++ logical_operator.rb
@@ -0,0 +1,13 @@
+puts("Please tell me your nickname!")
+in_str = gets.chomp()
+
+real_yooshink = "keroro"
+real_noah = "tamama"
+
+if in_str == real_yooshink || in_str == "yooshin"
+  puts("Hi Yooshin~ Ruby")
+elsif in_str == real_noah  # else if -> elsif
+  puts("Hi Noah~ Ruby")
+else
+  puts("who are you?")
+end
