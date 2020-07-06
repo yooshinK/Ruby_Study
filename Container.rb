e37f98742c84b64eab00e22ac375a172c8213361
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-07-06 23:58:04 +0900

    Add files via upload

---
+++ Container.rb
@@ -0,0 +1,25 @@
+# Result = String / .class 가 데이터형식을 체크
+name = 'yooshink'
+puts('yooshink')
+puts(name.class)
+
+# Result = Array
+print("Names - ")
+names = ['yooshin','TEST',"Noah"]
+puts(['yooshin','TEST',"Noah"].class)
+puts(names.class)
+puts(names)
+puts(names[0])
+puts(names[1])
+puts(names[2])
+
+print("A Person - ")
+person = ['Noah',20,TRUE] # true, false or TRUE, FALSE
+puts(['Noah',29,true].class)
+puts(person[2].class)
+puts(person)
+puts(person[0])
+puts(person[1])
+puts(person[2])
+person[1] +=  1
+puts(person[1])
