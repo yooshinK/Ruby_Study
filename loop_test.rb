04a30ceb7fa147be376e7788c0584a43dfff8efc
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-07-21 22:34:40 +0900

    Add files via upload

---
+++ loop_test.rb
@@ -0,0 +1,29 @@
+i = 0
+while i < 3 do # true, false
+  puts('HELLO')
+  puts('Hello '+i.to_s)
+  i += 1
+end
+puts('Hello '+i.to_s)
+puts('after while')
+
+puts('---------------------------')
+j = 0
+while j < 10
+  puts('puts("Hello World X '+(j*10).to_s+ ' ")')
+  j = j + 1
+end
+puts(j*10)
+
+puts('---------------------------')
+k = 0
+a = 0
+while k < 10 do
+  if k%2 == 0
+    a = a + 1
+  end
+  puts('puts("Hello World X '+(j*10).to_s+ ' ")')
+  k = k + 1
+end
+puts('after while')
+puts(a)
