d1f902a72b0dbca67c093b424af08f8e3aef7611
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-07-23 16:39:13 +0900

    Update container_loop.rb

--- container_loop.rb
+++ container_loop.rb
@@ -1,8 +1,23 @@
 members = ['keroro','dororo','tamama']
-
+members1 = ['keroro11','dororo22','tamama33']
 i = 0
 
+#while
 while i < members.length do
   print(members[i]+" ")
   i+=1
+end
+
+puts("\n-----------------------------")
+#for
+for j in 0..3 do
+  print(members1[j])
+  print(" ")
+end
+
+puts("\n-----------------------------")
+
+for member in members1 do
+  print(member)
+  print(" ")
 end
