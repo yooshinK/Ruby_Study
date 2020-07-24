8072661d4df4ef7f2c7c50fbabbaefe535239788
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-07-24 14:13:05 +0900

    Add files via upload

---
+++ login_function_miniproject.rb
@@ -0,0 +1,29 @@
+real_members = ['yooshin','noah','keroro','tamama','dororo','kiroro']
+real_id = "lifeandstory"
+real_pwd = "story"
+pwd_count = 5
+logged_in = false
+#Input
+puts("Please input your id")
+input_id = gets.chomp()
+
+for member in real_members do
+    if member == input_id
+        while pwd_count > 0 do
+            puts("Please input your password\n")
+            input_pwd = gets.chomp()
+            if real_pwd == input_pwd
+                puts("Welcome! "+ member)
+                logged_in = true
+                break
+            else
+                puts("Wrong Password! Count:"+pwd_count.to_s)
+                pwd_count -= 1
+            end
+        end
+    end
+end
+
+if (logged_in == false) and (pwd_count==5)
+    puts("Wrong ID")
+end
