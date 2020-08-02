8d404f86d84b7e6204d7f56cd8344bbfa10b8465
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-08-02 10:15:54 +0900

    Update ruby_block.rb

--- ruby_block.rb
+++ ruby_block.rb
@@ -6,10 +6,28 @@
 #   puts 'five'
 #   i += 1
 # end
-
-
-
 10.times() {puts 'two'}
 3.upto(5) {|i| print i, '-step '} # Result 3-step 4-step 5-step
 3.upto(5) {|i| print 'step2 '} # Result step2 step2 step2
 3.upto(5) {print 'step3 '} # Result step3 step3 step3
+
+puts("-------------------------------")
+#array block
+arr = ['A','B','C']
+arr.each() { |i| puts i }
+
+for value in arr
+ puts(arr)
+end
+
+puts("-------------------------------")
+#array block2
+arr2 = [1,3,56,7,13,52]
+
+# arr2.delete_if() do |item|
+#   puts item
+#   item > 7
+# end
+arr2.delete_if(){|item| item > 7}
+
+puts arr2
