51ac0e3917e18ee8b3f74f0819756a364bc5435b
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-08-04 00:02:08 +0900

    Add files via upload

---
+++ module_test.rb
@@ -0,0 +1,16 @@
+# more module info: https://ruby-doc.org/core-2.6/Math.html
+puts(2.1.ceil()) # Result = 3
+puts(2.9.ceil()) # Result = 3
+puts(2.1.floor()) # Result = 2
+puts(2.9.floor()) # Result = 2
+puts(Math.sqrt(15)) # Result = 3.872983346207417
+puts(3.872983346207417*3.872983346207417) # Result = 15.000000000000002
+
+
+num1 = -19.897
+num2 = -18.321
+num3 = 190.23213
+# Prints ceil() of num
+puts  num1.ceil(1)
+puts  num2.ceil(2)
+puts  num3.ceil(3)
