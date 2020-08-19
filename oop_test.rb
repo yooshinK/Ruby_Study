632972d542dc771cc6e631231b2bf23b9840dbd2
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-08-19 21:18:21 +0900

    Update oop_test.rb

--- oop_test.rb
+++ oop_test.rb
@@ -6,3 +6,15 @@ name2 = String.new('Noah')
 puts(name1.reverse())
 puts(name2.reverse())
 puts(names.reverse())
+
+puts(name1.upcase())
+puts(name1.size())
+
+puts(name2.upcase())
+puts(name2.size())
+
+names1 = Array.new()
+names1.push('hahaha', 'lololo')
+names1.push('gogogogogogogo')
+puts(names1)
+puts(names1.join("--")) # Result: hahaha--lololo--gogogogogogogo
