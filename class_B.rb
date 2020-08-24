79d10735ca4940aecc33d91c9427c114c379853d
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-08-24 20:20:08 +0900

    Add files via upload

---
+++ class_B.rb
@@ -0,0 +1,17 @@
+  def add(a,b)
+    return a+b
+  end
+
+  def subtract(a,b)
+    return a-b
+  end
+
+num1 = 10
+num2 = 7
+p add(num1,num2)
+p subtract(num1,num2)
+
+num3 = 30
+num4 = 3
+p add(num3,num4)
+p subtract(num3,num4)
