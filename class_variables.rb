d13368636907a9b851900679f7160b88128743fb
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-09-07 00:49:16 +0900

    Update class_variables.rb

--- class_variables.rb
+++ class_variables.rb
@@ -1,14 +1,15 @@
-class Cs
-  @@count = 0
+class Class_Variable_Test
+  @@count = 0 # 해당 클래스의 인스턴스가 모두 공유하는 변수
   def initialize()
-    @@count = @@count + 1
+    @@count += 1
   end
-  def Cs.getCount()
+  def Class_Variable_Test.getCount() # 인스턴스가 없어도 사용할 수 있는 클래스 함수
     return @@count
   end
 end
-i1 = Cs.new()
-i2 = Cs.new()
-i3 = Cs.new()
-i4 = Cs.new()
-p Cs.getCount()
+
+# i1 = Class_Variable_Test.new()
+# i2 = Class_Variable_Test.new()
+# i3 = Class_Variable_Test.new()
+
+p Class_Variable_Test.getCount()
