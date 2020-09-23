b9655ae06f85714ba5e96e9efb43e61538b9c5d1
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-09-23 21:22:41 +0900

    Update package_manager.rb

--- package_manager.rb
+++ package_manager.rb
@@ -7,6 +7,10 @@ p r
 
 page = Nokogiri::HTML(r)
 puts page.class   # => Nokogiri::HTML::Document
+puts page.css("title")[0].name   # => title
+puts page.css("title")[0].text   # => Magazine - AMANG Korea
+p "------------------------------------------"
 puts "title: " + page.css('title')[0].text
 puts "Article list: " ++ page.css('a')[0].text
 puts "link: "+ page.css('a')[0]['href']
+puts "Class: "+ page.css('div.magazine_page')[0] # class 안에 있는 정보들 다 나옴
