49120d6376c1d6a8787d26d1e9cd8e19bb716fa2
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-10-14 23:21:35 +0900

    Add files via upload

---
+++ Algorithm_rb_absolute_number.rb
@@ -0,0 +1,18 @@
+import math
+
+def abs_sign(n)
+    if n > 0
+        return n
+    end
+    else if n <= 0
+        return -n
+    end
+end
+def abs_square(n)
+    a = n * n
+    return math.isqrt(a) # return result as float
+    # math.isqrt(n)	Returns the nearest integer square root of n
+end
+
+print(abs_sign(-3))
+print(abs_square(5))
